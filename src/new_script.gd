59efab09930eaee59d1da307ba7046e719b92f8c
danny-karlsson-ju <kada25lb@student.ju.se>
2026-03-14 18:28:43 +0100

    setup gdquest formatter

--- src/new_script.gd
+++ src/new_script.gd
@@ -1,5 +1,6 @@
 extends Node
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -7,7 +8,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass 
-	
+	pass
+
+
 func test():
 	pass
