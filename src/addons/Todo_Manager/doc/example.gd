46b892ba55445e0213f6fe6b730eaf1ff88ab7ac
danny-karlsson-ju <kada25lb@student.ju.se>
2026-03-14 18:20:01 +0100

    setup todomanager

--- src/addons/Todo_Manager/doc/example.gd
+++ src/addons/Todo_Manager/doc/example.gd
@@ -3,5 +3,7 @@ extends Node
 # TODO: this is a TODO
 # HACK: this is a HACK
 # FIXME: this is a FIXME
+# OPT: this is a OPT
+# NEXT: this is a NEXT
 #      TODO          this works too
 #Hack any format will do
