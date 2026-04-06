25850b8ed51600a7578f05bcbbb5e5c7b755c8e9
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-04-06 11:59:49 +0200

    test different ways to handle data i like resources most fits godot style and gives suggestions

--- src/src/routes/documentation/gym/gym.gd
+++ src/src/routes/documentation/gym/gym.gd
@@ -4,4 +4,9 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	DataManager.load_data()
+	print("DataManager exaple: " + str(DataManager.payload.video.fullscreen))
+	DataManager.payload.video.fullscreen = !DataManager.payload.video.fullscreen
+	DataManager.save_data()
+	
+	print("DataManager exaple: " + str(DataManager.payload.video.fullscreen))
