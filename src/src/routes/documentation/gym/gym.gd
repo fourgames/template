e7f59a238521023f977b5ea0ecf56693ca452453
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-04-06 12:46:48 +0200

    organize and yeah

--- src/src/routes/documentation/gym/gym.gd
+++ src/src/routes/documentation/gym/gym.gd
@@ -4,6 +4,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pass
 	DataManager.load_data()
 	print("DataManager exaple: " + str(DataManager.payload.video.fullscreen))
 	DataManager.payload.video.fullscreen = !DataManager.payload.video.fullscreen
