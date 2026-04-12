dc89ebe1126014322476554ed1ddca3bbd03ce8f
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-04-12 15:44:16 +0200

    remove a little more unessesary stuff

--- src/src/routes/documentation/gym/gym.gd
+++ src/src/routes/documentation/gym/gym.gd
@@ -1,6 +1,9 @@
 @icon("uid://xrg0p8m7ymeb")
 extends Node3D
 
+## Gyms, Zoos, and Museums: Your documentation should be in-game by Robin-Yann Storm
+## - https://www.youtube.com/watch?v=5PJRCz0t7yY
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
