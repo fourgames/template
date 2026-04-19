160167c255708bd4b594064bc892f857323c65cc
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-04-19 11:16:48 +0200

    Call AnimationPlayer directly, remove onready var
    Remove the @onready animation_player variable and use the %AnimationPlayer shorthand to play and await the 'boot_splash_animation'. Behavior is unchanged: debug builds still immediately change to GYM_SCENE, non-debug builds play the animation then change scenes.

--- src/src/components/menues/boot_splash/boot_splash.gd
+++ src/src/components/menues/boot_splash/boot_splash.gd
@@ -1,11 +1,10 @@
 extends CanvasLayer
 
-@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 func _ready() -> void:
 	if OS.is_debug_build():
 		SceneChanger.change_scene(PathManager.GYM_SCENE)
 	else:
-		animation_player.play("boot_splash_animation")
-		await animation_player.animation_finished
+		%AnimationPlayer.play("boot_splash_animation")
+		await %AnimationPlayer.animation_finished
 		SceneChanger.change_scene(PathManager.GYM_SCENE)
