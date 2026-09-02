862048861133ed0b721c7e076cd6ed79da7be623
danny-karlsson-ju <ytdannykarlsson@gmail.com>
2026-09-02 12:03:50 +0200

    Update credits_data.gd

--- core/data_manager/resources/credits_data.gd
+++ core/data_manager/resources/credits_data.gd
@@ -3,9 +3,9 @@ class_name CreditsData
 
 @export var credits_list : Array[Dictionary] = [
 	{"name": "Danny", "role": "Director"},
-	{"name": "Godot Engine", "role": "Game Engine"},
-	{"name": "Blender", "role": "3D Modeling"},
-	{"name": "Lorc, Delapouite & contributors", "role": "Icons (game-icons.net)"},
-	{"name": "Zapsplat", "role": "Audio"},
+	{"name": "Godot Engine", "role": "Game Engine (godotengine.org)"},
+	{"name": "Blender", "role": "3D Modeling (blender.org)"},
+	{"name": "Lorc, Delapouite & contributors", "role": "Game Icons (game-icons.net)"},
+	{"name": "Zapsplat", "role": "Audio & Sound Effects (zapsplat.com)"},
 	{"name": "You", "role": "Thanks for playing!"},
 ]
